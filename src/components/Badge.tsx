46c0c2db3991ba7f3830116f8d6829afbdd0808d
truemassimo <root@DESKTOP-7MB3NJJ>
2024-11-03 10:52:40 +0100

    customized powerd by

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Regione Marche - Polo Strategico Regionale'}</span>            
+            <span>&nbsp;{props.footer?.company ?? 'Regione Marche - Polo Strategico Regionale'}</span>
           </a>
         </span>
       </Show>
